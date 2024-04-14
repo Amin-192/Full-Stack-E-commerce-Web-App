db2201ae35f984653d0a5bd329255c52f77cd9ef
AMINSTYLEZ <aminstylez19@gmail.com>
2024-04-14 16:27:44 +0300

    working my way onto making it a full stack app

--- shopping/src/assets/components/Hero.jsx
+++ shopping/src/assets/components/Hero.jsx
@@ -3,6 +3,7 @@ import shoe2 from '../images/airforce.png';
 import shoe3 from '../images/airmax97.png';
 import shoe4 from '../images/shoe4.png';
 import shoe5 from '../images/airofce1.png';
+import { useState } from 'react';
 
 
 const Hero = () => {
@@ -35,8 +36,20 @@ price : 4100
          },
 
     ]
+   
+        const [div, setDiv] = useState('none');
+    
+        const handleDiv = () => {
+            setDiv(div === 'none' ? 'block' : 'none');
+        };
 
+        const [addCart , setCart] = useState([]);
 
+        const handleCart = () =>{
+            setCart([...addCart, products[0]]);  
+            
+
+            }
   return (
     <div className='h-[700px]'>
      
@@ -48,10 +61,20 @@ price : 4100
                         <img className='relative left-[20%] w-[200px] h-[200px]' src={product.image} alt={product.name}  />
                         <h2 className='text-center text-2xl font-serif font-bold '>{product.name}</h2>
                         <p className='text-xl text-center p-1 mt-4'><span className='text-orange-300 '>Price:</span>  ksh{product.price}</p>
-                        <button className='shadow-2xl p-4 hover:bg-orange-300 relative left-[40%] mt-3 font-serif  rounded-xl hover:scale-110 transition-all duration-300'>BUY</button>
+                        <button onClick={handleCart} className='shadow-2xl p-4 hover:bg-orange-300 relative left-[40%] mt-3 font-serif  rounded-xl hover:scale-110 transition-all duration-300'>BUY</button>
                     </div>
                 ))}
             </ul>
+
+            <div className='displayDiv'>
+                    <div className="h-[500px] bg-gray-600 w-[500px] top-[30%] absolute" style={{ display: div }}>
+                        <h1>cart</h1>
+                    </div>
+                    </div>
+            <button onClick={handleDiv} className="relative left-[70%]  text-xl hover:bg-orange-300  shadow-2xl p-4 rounded-xl hover:scale-110 transition-all duration-300 w-[100px] ">
+                        Cart 🛒
+                    </button>
+                    
     </div>
   )
 }
--- shopping/src/assets/components/Time.jsx
+++ shopping/src/assets/components/Time.jsx
@@ -1,16 +1,39 @@
+import { useEffect, useState } from "react"
 
 
 const Time = () => {
+    const [time ,  setTime] = useState(new Date());
+
+useEffect(()=> {
+ const interval = setInterval(() => {
+     setTime(new Date());
+ }, 1000);
+ return () => clearInterval(interval);
+},[]);
+
+function formatTime(){
+    let hours = time.getHours();
+    let minuets = time.getMinutes();
+    let seconds = time.getSeconds();
+    let meridiam = hours >= 12 ? 'PM' : 'AM';
+    
+    return `${padZero(hours)}:${padZero(minuets)}: ${padZero(seconds)}: ${meridiam}`  
+}
+function padZero(number){
+return (number < 10 ? '0' : '') + number;
+}
   return (
     <div className="mt-[160px] h-[400px] ">
-      <h1>this is from the times component </h1>
+      <h1>NEW DEALS ARE RUNNING OUT</h1>
+
 
 
       <div className="clock-container">
 
-<div className="clock">
+<div className="clock text-center p-3 shadow-2xl rounded-2xl ml-[70px] mr-[70px] mt-5">
 
-    <span></span>
+    <span className="text-8xl p-3 ">{formatTime()}</span>
+    <h1></h1>
 </div>
       </div>
     </div>
